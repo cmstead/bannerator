1a2a2fa95f79a82ea53bf45c2ba9d670ed0e2cf7
Chris Stead <cmstead@gmail.com>
2019-09-19 10:04:04 -0700

    Initial implementation of bannerator

---
+++ index.js
@@ -0,0 +1,53 @@
+function Bannerator() {
+    this.bannerText = ['Bannerator loves you!'];
+    this.bannerSurround = '*';
+}
+
+Bannerator.prototype = {
+    setBannerText: function (bannerText) {
+        if(typeof bannerText === 'string') {
+            this.bannerText = [bannerText];
+        } else if(Array.isArray(bannerText)) {
+            this.bannerText = bannerText;
+        } else {
+            throw new TypeError('Banner text must be a a string or an array');
+        }
+    },
+
+    setBannerSurround: function (bannerSurround) {
+        if(typeof bannerSurround === 'string') {
+            this.bannerSurround = bannerSurround;
+        } else {
+            throw new TypeError('Banner surround text must be a string');
+        }
+    },
+
+    buildBannerSurroundValue: function () {
+        let surroundValue = '';
+        const repeatCount = Math.floor(80 / this.bannerSurround.length);
+
+        for(let i = 0; i <= repeatCount; i++) {
+            surroundValue += this.bannerSurround;
+        }
+
+        return surroundValue;
+    },
+
+    displayBanner: function () {
+        const randomIndex = Math.floor(Math.random() * this.bannerText.length);
+        const bannerItem = this.bannerText[randomIndex];
+        const bannerSurroundValue = this.buildBannerSurroundValue();
+
+        console.log(bannerSurroundValue);
+        console.log(bannerItem);
+        console.log(bannerSurroundValue);
+    }
+};
+
+function buildBannerator() {
+    return new Bannerator();
+}
+
+module.exports = {
+    buildBannerator
+};
